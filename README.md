7c008428ef4d359ca941202fc310a7152709fac6
csesarah <63842047+csesarah@users.noreply.github.com>
2025-04-30 13:00:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,138 @@
+# ME5415 AY2024/2025 Project 
+
+# Pipe Robot Locomotion
+After initial environment setup steps, in the ```/proj``` directory
+
+        ./sofa/build/bin/runSofa src/main.py
+
+---
+
+# Environment Setup
+
+## Install SOFA
+
+### Dependencies
+
+        sudo apt upgrade && sudo apt update
+        sudo apt install git curl
+        sudo apt install build-essential software-properties-common
+        sudo apt install gcc-12
+        sudo apt install clang-15
+        sudo apt install ccache
+        sudo apt install cmake cmake-gui
+        sudo apt install libopengl0 
+        sudo apt install python3-pip pybind11-dev
+        sudo apt install libtinyxml2-dev
+        sudo apt install libboost-all-dev
+        sudo apt install libeigen3-dev
+        sudo apt install xorg-dev libgtk-3-dev
+        sudo apt install libgl1-mesa-dev
+        sudo apt install libpng-dev libjpeg-dev libtiff-dev libglew-dev zlib1g-dev
+        sudo apt install nvidia-cuda-toolkit
+        sudo apt install -y qtcreator qtbase5-dev qt5-qmake 
+        sudo apt install libgtest-dev
+        python3 -m pip install pip==21.0
+        python3 -m pip install numpy==1.26.4 scipy pybind11==2.9.1
+
+### Download
+Download SOFA repo and set up file directory structure
+
+        git clone -b v23.06 https://github.com/sofa-framework/sofa.git sofa/src
+        cd sofa
+        mkdir build
+
+Download plugins into ```plugins``` directory
+
+        mkdir plugins
+        cd plugins
+        git clone https://github.com/SofaDefrost/SPLIB
+        git clone -b v23.06 https://github.com/SofaDefrost/STLIB
+        git clone -b v23.06 https://github.com/SofaDefrost/SoftRobots
+        git clone -b v23.06 https://github.com/SofaDefrost/ModelOrderReduction
+        git clone -b v23.06 https://github.com/SofaDefrost/Cosserat
+        git clone -b v23.06 https://github.com/sofa-framework/BeamAdapter
+
+### Build
+
+1. Create ```CMakeLists.txt``` file in ```plugins``` directory as follows:
+
+        cmake_minimum_required(VERSION 3.22)
+
+        find_package(SofaFramework)
+
+        sofa_add_subdirectory(plugin Cosserat/  Cosserat)
+        sofa_add_subdirectory(plugin ModelOrderReduction/  ModelOrderReduction)
+        sofa_add_subdirectory(plugin SoftRobots/  SoftRobots)
+        sofa_add_subdirectory(plugin SPLIB/  SPLIB)
+        sofa_add_subdirectory(plugin STLIB/  STLIB)
+
+2. Generate Makefile
+
+       cmake-gui
+
+3. Set generator as ```Unix Makefiles``` and select ```Use default native compilers```
+4. Set source folder as ```sofa/src``` and build folder as ```sofa/build```
+5. Run **Configure** and set the following parameters:
+
+        SOFA_FETCH_BEAMADAPTER=ON
+        SOFA_FETCH_SOFAPYTHON3=ON
+        SOFA_EXTERNAL_DIRECTORIES=/PATH_TO/proj/plugins
+
+6. Run **Configure** again and ensure that the following parameters are set as follows:
+
+        PLUGIN_SOFAPYTHON3=ON
+        PLUGIN_BEAMADAPTER=ON
+        PLUGIN_COSSERAT=ON
+        PLUGIN_SPLIB=ON
+        PLUGIN_STLIB=ON
+        PLUGIN_SOFTROBOTS=ON
+        PLUGIN_MODELORDERREDUCTION=ON
+
+7. Run **Generate**
+8. Build
+
+        cd build
+        make
+
+9. Load plugins
+
+        cp lib/plugin_list.conf.default lib/plugin_list.conf
+        ls lib | grep plugin
+        echo -e "\nSofaPython3 NO_VERSION" >> lib/plugin_list.conf
+        cat lib/plugin_list.conf
+
+### Run
+
+     ./bin/runSofa
+
+---
+
+## Install SofaGym
+
+### Download
+
+        git clone https://github.com/SofaDefrost/SofaGym
+
+### Dependencies
+
+        python3 -m pip install setuptools==65.5.0 
+        python3 -m pip install wheel==0.38.0
+        python3 -m pip install gym==0.21.0
+        python3 -m pip install stable-baselines3[extra]==1.7.0
+        python3 -m pip install rlberry==0.5.0
+        python3 -m pip install psutil pygame glfw pyopengl imageio imageio-ffmpeg
+        python3 -m pip install tensorboard
+
+        export SOFA_ROOT=$(pwd)/sofa/build/
+        export PYTHONPATH=$(pwd)/sofa/build/lib/python3/site-packages:$PYTHONPATH
+
+        cd SofaGym
+        python3 setup.py bdist_wheel
+        python3 -m pip install -v -e .
+
+### Run
+
+        python3 test_env.py -e trunk-v0 -ep 100 -s 100
+        python3 rl.py -e trunk-v0 -a PPO
+
+
---
+++ src/main.py
@@ -0,0 +1,217 @@
+import math
+
+youngModulusActuators = 500
+youngModulusStiffLayerActuators = 1500
+
+ROBOT_POS = [-10, 0, 0]
+ROBOT_ORT = [0, 0, -90]
+
+def createScene(rootNode):
+    rootNode.addObject('RequiredPlugin',
+                       pluginName='SoftRobots SofaPython3')
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.AnimationLoop')  # Needed to use components [FreeMotionAnimationLoop]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Collision.Detection.Algorithm')  # Needed to use components [BVHNarrowPhase,BruteForceBroadPhase,CollisionPipeline]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Collision.Detection.Intersection')  # Needed to use components [LocalMinDistance]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Collision.Geometry')  # Needed to use components [LineCollisionModel,PointCollisionModel,TriangleCollisionModel]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Collision.Response.Contact')  # Needed to use components [CollisionResponse]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Lagrangian.Correction')  # Needed to use components [GenericConstraintCorrection,UncoupledConstraintCorrection]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Constraint.Lagrangian.Solver')  # Needed to use components [GenericConstraintSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Engine.Select')  # Needed to use components [BoxROI]  
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.IO.Mesh')  # Needed to use components [MeshOBJLoader,MeshSTLLoader,MeshVTKLoader]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.LinearSolver.Direct')  # Needed to use components [SparseLDLSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.LinearSolver.Iterative')  # Needed to use components [CGLinearSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mapping.Linear')  # Needed to use components [BarycentricMapping]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mapping.NonLinear')  # Needed to use components [RigidMapping]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Mass')  # Needed to use components [UniformMass]  
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.ODESolver.Backward')  # Needed to use components [EulerImplicitSolver]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Setting')  # Needed to use components [BackgroundSetting]  
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.SolidMechanics.FEM.Elastic')  # Needed to use components [TetrahedronFEMForceField]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.SolidMechanics.Spring')  # Needed to use components [RestShapeSpringsForceField]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.StateContainer')  # Needed to use components [MechanicalObject]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Constant')  # Needed to use components [MeshTopology]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Topology.Container.Dynamic')  # Needed to use components [TetrahedronSetTopologyContainer]
+    rootNode.addObject('RequiredPlugin', name='Sofa.Component.Visual')  # Needed to use components [VisualStyle]  
+    rootNode.addObject('RequiredPlugin', name='Sofa.GL.Component.Rendering3D')  # Needed to use components [OglModel,OglSceneFrame]
+
+    rootNode.addObject('VisualStyle',
+                       displayFlags='showVisualModels hideBehaviorModels hideCollisionModels hideBoundingCollisionModels hideForceFields showInteractionForceFields hideWireframe')
+    rootNode.gravity.value = [-9810, 0, 0]
+    rootNode.addObject('FreeMotionAnimationLoop')
+    rootNode.addObject('GenericConstraintSolver', tolerance=1e-12, maxIterations=10000)
+    # rootNode.addObject('EulerImplicitSolver', name='odesolver', rayleighStiffness=0.1, rayleighMass=0.1)
+    rootNode.addObject('DefaultPipeline')
+    rootNode.addObject('BruteForceBroadPhase')
+    rootNode.addObject('BVHNarrowPhase')
+    rootNode.addObject('DefaultContactManager', response='FrictionContactConstraint', responseParams='mu=0.6')
+    rootNode.addObject('LocalMinDistance', name='Proximity', alarmDistance=5, contactDistance=1, angleCone=0.0)
+
+    rootNode.addObject('BackgroundSetting', color=[0, 0.168627, 0.211765, 1.])
+    rootNode.addObject('OglSceneFrame', style='Arrows', alignment='TopRight')
+
+
+    ##########################################
+    ##### Actuator
+    ##########################################
+    
+    actuator = rootNode.addChild('Actuator 1')
+    actuator.addObject('EulerImplicitSolver', name='odesolver', rayleighStiffness=0.1, rayleighMass=0.1)
+    actuator.addObject('SparseLDLSolver', name='preconditioner', template="CompressedRowSparseMatrixMat3x3d")
+
+    actuator.addObject('MeshVTKLoader', name='loader', filename='data/mesh/pneunetCutCoarse.vtk',
+                        translation=ROBOT_POS, rotation=ROBOT_ORT)
+    # actuator.addObject('MeshTopology', src='@loader', name='container')
+    actuator.addObject('TetrahedronSetTopologyContainer', position='@loader.position', 
+                    tetrahedra='@loader.tetrahedra', name='container')
+
+    actuator.addObject('MechanicalObject', name='tetras', template='Vec3', 
+                    showIndices=False, showIndicesScale=4e-5)
+    actuator.addObject('UniformMass', totalMass=0.04)
+    actuator.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large', poissonRatio=0.3,
+                        youngModulus=youngModulusActuators)
+
+    # actuator.addObject('BoxROI', name='boxROI', box=[-10, 0, -20, 0, 30, 20], drawBoxes=True, doUpdate=True)
+    # actuator.addObject('BoxROI', name='boxROISubTopo', box=[-100, 22.5, -8, -19, 28, 8], drawBoxes=True, strict=False, computeTetrahedra=False)
+    # actuator.addObject('BoxROI', name='boxROISubTopo', box='0 0 0 150 -100 1', drawBoxes=True)
+    # actuator.addObject('BoxROI', name='membraneROISubTopo', box='0 0 -0.1 150 -100 0.1', computeTetrahedra="false",
+    #                    drawBoxes=True)
+    # actuator.addObject('RestShapeSpringsForceField', points=0, stiffness=1e12,
+    #                     angularStiffness=1e12)
+    actuator.addObject('LinearSolverConstraintCorrection')
+
+    ##########################################
+    # Sub topology						   #
+    ##########################################
+    modelSubTopo = actuator.addChild('modelSubTopo')
+    modelSubTopo.addObject('TriangleSetTopologyContainer', position='@../boxROISubTopo.pointsInROI',
+                            triangles='@../boxROISubTopo.trianglesInROI', name='containerSub')
+    modelSubTopo.addObject('TriangleFEMForceField', template='Vec3d', name='FEM', method='large',
+                            poissonRatio=0.4, youngModulus=youngModulusStiffLayerActuators - youngModulusActuators)
+    # modelSubTopo.addObject('TetrahedronSetTopologyContainer', position='@loader.position',
+    #                         tetrahedra='@boxROISubTopo.tetrahedraInROI', name='containerSub')
+    # modelSubTopo.addObject('TetrahedronFEMForceField', template='Vec3', name='FEM', method='large',
+    #                         poissonRatio=0.3, youngModulus=youngModulusStiffLayerActuators - youngModulusActuators)
+
+    ##########################################
+    # Constraint							 #
+    ##########################################
+    cavity = actuator.addChild('cavity')
+    cavity.addObject('MeshSTLLoader', name='loader', filename='data/mesh/pneunetCavityCut.stl',
+                        translation=ROBOT_POS, rotation=ROBOT_ORT)
+    cavity.addObject('MeshTopology', src='@loader', name='topo')
+    cavity.addObject('MechanicalObject', name='cavity')
+    cavity.addObject('SurfacePressureConstraint', name='SurfacePressureConstraint', template='Vec3', value=0.0001,
+                        triangles='@topo.triangles', valueType='pressure')
+    cavity.addObject('BarycentricMapping', name='mapping', mapForces=False, mapMasses=False)
+
+    ##########################################
+    # Collision							  #
+    ##########################################
+
+    collisionActuator = actuator.addChild('collisionActuator')
+    collisionActuator.addObject('MeshSTLLoader', name='loader', filename='data/mesh/pneunetCut.stl',
+                                translation=ROBOT_POS, rotation=ROBOT_ORT)
+    collisionActuator.addObject('TriangleSetTopologyContainer', position='@loader.position', 
+                                triangles='@loader.triangles', name='container_actuator')
+    # collisionActuator.addObject('MeshTopology', src='@loader', name='topo')
+    collisionActuator.addObject('MechanicalObject', name='collisMech', template='Vec3d')
+    collisionActuator.addObject('TriangleCollisionModel', selfCollision=False)
+    collisionActuator.addObject('LineCollisionModel', selfCollision=False)
+    collisionActuator.addObject('PointCollisionModel', selfCollision=False)
+    collisionActuator.addObject('BarycentricMapping')
+
+    ##########################################
+    ##### Visualization
+    ##########################################
+    modelVisu = actuator.addChild('Visu')
+    modelVisu.addObject('MeshSTLLoader', name='loader', filename='data/mesh/pneunetCut.stl', translation=ROBOT_POS, rotation=ROBOT_ORT)
+    modelVisu.addObject('OglModel', src='@loader', color=[0.7, 0.7, 0.7, 0.6], template='Vec3')
+    modelVisu.addObject('BarycentricMapping')
+
+    ##########################################
+    #### Floor
+    ##########################################
+
+    planeNode = rootNode.addChild('Plane')
+    planeNode.addObject('MeshOBJLoader', name='loader', filename='data/mesh/floorFlat.obj', triangulate=True,
+                        rotation=[0, 0, 270], scale=10, translation=[-15, 0, 0])
+    planeNode.addObject('MeshTopology', src='@loader')
+    planeNode.addObject('MechanicalObject', src='@loader')
+    planeNode.addObject('TriangleCollisionModel', simulated=False, moving=False)
+    planeNode.addObject('LineCollisionModel', simulated=False, moving=False)
+    planeNode.addObject('PointCollisionModel', simulated=False, moving=False)
+    planeNode.addObject('OglModel', name='Visual', src='@loader', color=[1, 0, 0, 1])
+
+    ## Cube to check collisions/gravity
+    cube = rootNode.addChild('cube')
+    cube.addObject('EulerImplicitSolver', name='odesolver')
+    cube.addObject('SparseLDLSolver', name='linearSolver', template="CompressedRowSparseMatrixMat3x3d")
+    cube.addObject('MechanicalObject', template='Rigid3', position=[20, 70, 70, 0, 0, 0, 1])
+    cube.addObject('UniformMass', totalMass=0.001)
+    cube.addObject('UncoupledConstraintCorrection')
+
+    # collision
+    cubeCollis = cube.addChild('cubeCollis')
+    cubeCollis.addObject('MeshOBJLoader', name='loader', filename='data/mesh/smCube27.obj', triangulate=True,
+                         scale=6)
+    cubeCollis.addObject('MeshTopology', src='@loader')
+    cubeCollis.addObject('MechanicalObject')
+    cubeCollis.addObject('TriangleCollisionModel')
+    cubeCollis.addObject('LineCollisionModel')
+    cubeCollis.addObject('PointCollisionModel')
+    cubeCollis.addObject('RigidMapping')
+
+    # visualization
+    cubeVisu = cube.addChild('cubeVisu')
+    cubeVisu.addObject('MeshOBJLoader', name='loader', filename='data/mesh/smCube27.obj')
+    cubeVisu.addObject('OglModel', name='Visual', src='@loader', color=[0.0, 0.1, 0.5], scale=6.2)
+    cubeVisu.addObject('RigidMapping')
+    
+    rootNode.addObject(RobotController(node=rootNode))
+
+    return rootNode
+
+'''
+===
+'''
+
+import Sofa.Core
+from Sofa.constants import *
+import math
+
+class RobotController(Sofa.Core.Controller):
+
+    def __init__(self, *a, **kw):
+
+        Sofa.Core.Controller.__init__(self, *a, **kw)
+        self.node = kw["node"]
+        self.constraints = []
+        self.dofs = []
+        self.dofs.append(self.node.getChild('Actuator 1').getMechanicalState())
+        self.constraints.append(self.node.getChild('Actuator 1').cavity.SurfacePressureConstraint)
+
+        self.increasing = True
+
+        return
+
+    def onAnimateBeginEvent(self, event):
+        # print("onAnimateBeginEvent")
+
+        increment = 0.05
+        pressureHighThreshold = 1.2
+        pressureLowThreshold = 0.2
+
+        if self.increasing or self.constraints[0].value.value[0] < pressureHighThreshold:
+            pressureValue = self.constraints[0].value.value[0] + increment
+            if pressureValue > 1.5: # max pressure edge case
+                pressureValue = 1.5
+            self.constraints[0].value = [pressureValue]
+            self.increasing = False
+        else:
+            if not self.increasing:
+                while self.constraints[0].value.value[0] > pressureLowThreshold:
+                    pressureValue = self.constraints[0].value.value[0] - increment
+                    if pressureValue < 0: # min pressure edge case
+                        pressureValue = 0
+                    self.constraints[0].value = [pressureValue]
+
+        # print(self.constraints[0].value.value[0])
